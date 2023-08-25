5f352782e217ff574baff8bed251b77ffd71cddc
Rafael Prats <34110199+RafaelPrats@users.noreply.github.com>
2023-08-25 14:46:07 -0500

    rafa

--- resources/views/adminlte/gestion/postcocecha/ingreso_clasificacion/partials/pdf_etiqueta.blade.php
+++ resources/views/adminlte/gestion/postcocecha/ingreso_clasificacion/partials/pdf_etiqueta.blade.php
@@ -1,5 +1,5 @@
 @for ($i = 1; $i <= $datos['cantidad']; $i++)
-    <table style="position: relative; font-family: Arial, Helvetica, sans-serif;">
+    <table style="position: relative; top: -10px; left: 10px; font-family: Arial, Helvetica, sans-serif;">
         <tr style="padding: 0">
             <th style="text-align: center; padding: 0;">
                 {!! $barCode->getBarcode($datos['inventario_frio']->id_inventario_frio, $barCode::TYPE_CODE_128, 2) !!}
@@ -8,18 +8,18 @@
                 {{ $datos['variedad']->nombre }}
             </th>
             <td style="text-align: left;">
-                <b>{{ $datos['longitud']->nombre }}</b><small><sup>cm</sup></small>
+                <b>{{ $datos['longitud']->nombre }}<small><sup>cm</sup></small></b>
                 <br>
-                {{ $datos['tallos_x_ramo'] }}<small><sup>stems</sup></small>
+                <b>{{ $datos['tallos_x_ramo'] }}<small><sup>stems</sup></small></b>
             </td>
         </tr>
         <tr style="padding: 0">
-            <td style="text-align: left; font-size: 10px; padding: 0">
+            <th style="text-align: left; font-size: 10px; padding: 0">
                 {{ getDias(TP_LETRA)[transformDiaPhp(date('w', strtotime($datos['fecha'])))] }}{{ intVal(substr($datos['fecha'], 5, 2)) }}.{{ substr($datos['fecha'], 8, 2) }}
-            </td>
-            <td style="text-align: center; font-size: 10px; padding: 0" colspan="2">
+            </th>
+            <th style="text-align: center; font-size: 10px; padding: 0" colspan="2">
                 PRODUCT of ECUADOR
-            </td>
+            </th>
     </table>
 
     @if ($i < $datos['cantidad'])
