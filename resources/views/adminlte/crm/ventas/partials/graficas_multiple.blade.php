2f7f7f7da376fe4cba8386b7ee2a1352a840ece2
RafaelPrats <rafael.pratsrecasen@gmail.com>
2024-02-22 20:18:34 -0500

    rafa

--- resources/views/adminlte/crm/ventas/partials/graficas_multiple.blade.php
+++ resources/views/adminlte/crm/ventas/partials/graficas_multiple.blade.php
@@ -20,12 +20,150 @@
     <div class="tab-content no-padding">
         <div class="chart tab-pane active" id="monto-chart" style="position: relative">
             <canvas id="chart_monto" width="100%" height="40" style="margin-top: 5px"></canvas>
+
+            @php
+                $total_monto = 0;
+                foreach ($data as $d) {
+                    foreach ($d['valores'] as $item) {
+                        $total_monto += $item->monto;
+                    }
+                }
+            @endphp
+            <div class="row">
+                @foreach ($data as $pos_d => $d)
+                    @php
+                        $total_longitud = 0;
+                        foreach ($d['valores'] as $item) {
+                            $total_longitud += $item->monto;
+                        }
+                        $porcentaje = porcentaje($total_longitud, $total_monto, 1);
+                    @endphp
+                    <div class="col-sm-2 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-green">
+                                <b>{{ $porcentaje }}%</b>
+                            </span>
+                            <h5 class="description-header">
+                                <b>${{ number_format($total_longitud, 2) }}</b>
+                            </h5>
+                            <span class="description-text">
+                                <b>{{ $d['longitud'] }}cm</b>
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="col-sm-2 col-xs-6">
+                    <div class="description-block border-right">
+                        <span class="description-percentage text-green">
+                            <b>100%</b>
+                        </span>
+                        <h5 class="description-header">
+                            <b>${{ number_format($total_monto, 2) }}</b>
+                        </h5>
+                        <span class="description-text">
+                            <b>TOTAL</b>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="chart tab-pane" id="tallos-chart" style="position: relative">
             <canvas id="chart_tallos" width="100%" height="40" style="margin-top: 5px"></canvas>
+
+            @php
+                $total_tallos = 0;
+                foreach ($data as $d) {
+                    foreach ($d['valores'] as $item) {
+                        $total_tallos += $item->tallos;
+                    }
+                }
+            @endphp
+            <div class="row">
+                @foreach ($data as $pos_d => $d)
+                    @php
+                        $total_longitud = 0;
+                        foreach ($d['valores'] as $item) {
+                            $total_longitud += $item->tallos;
+                        }
+                        $porcentaje = porcentaje($total_longitud, $total_tallos, 1);
+                    @endphp
+                    <div class="col-sm-2 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-green">
+                                <b>{{ $porcentaje }}%</b>
+                            </span>
+                            <h5 class="description-header">
+                                <b>{{ number_format($total_longitud) }}</b>
+                            </h5>
+                            <span class="description-text">
+                                <b>{{ $d['longitud'] }}cm</b>
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="col-sm-2 col-xs-6">
+                    <div class="description-block border-right">
+                        <span class="description-percentage text-green">
+                            <b>100%</b>
+                        </span>
+                        <h5 class="description-header">
+                            <b>{{ number_format($total_tallos) }}</b>
+                        </h5>
+                        <span class="description-text">
+                            <b>TOTAL</b>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
         <div class="chart tab-pane" id="ramos-chart" style="position: relative">
             <canvas id="chart_ramos" width="100%" height="40" style="margin-top: 5px"></canvas>
+
+            @php
+                $total_ramos = 0;
+                foreach ($data as $d) {
+                    foreach ($d['valores'] as $item) {
+                        $total_ramos += $item->ramos;
+                    }
+                }
+            @endphp
+            <div class="row">
+                @foreach ($data as $pos_d => $d)
+                    @php
+                        $total_longitud = 0;
+                        foreach ($d['valores'] as $item) {
+                            $total_longitud += $item->ramos;
+                        }
+                        $porcentaje = porcentaje($total_longitud, $total_ramos, 1);
+                    @endphp
+                    <div class="col-sm-2 col-xs-6">
+                        <div class="description-block border-right">
+                            <span class="description-percentage text-green">
+                                <b>{{ $porcentaje }}%</b>
+                            </span>
+                            <h5 class="description-header">
+                                <b>{{ number_format($total_longitud) }}</b>
+                            </h5>
+                            <span class="description-text">
+                                <b>{{ $d['longitud'] }}cm</b>
+                            </span>
+                        </div>
+                    </div>
+                @endforeach
+                <div class="col-sm-2 col-xs-6">
+                    <div class="description-block border-right">
+                        <span class="description-percentage text-green">
+                            <b>100%</b>
+                        </span>
+                        <h5 class="description-header">
+                            <b>{{ number_format($total_ramos) }}</b>
+                        </h5>
+                        <span class="description-text">
+                            <b>TOTAL</b>
+                        </span>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -95,7 +233,7 @@
                 },
                 legend: {
                     display: true,
-                    position: 'bottom',
+                    position: 'top',
                     fullWidth: false,
                     /*onClick: function() {},
                     onHover: function() {},*/
